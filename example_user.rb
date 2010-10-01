3b6ef5f3aab45a3679cd6ad409d610786c1a8b56
Rudi Luzar <rluzar@topmill.ca>
2010-10-01 08:47:34 -0400

    Completed Ch4. Added blueprint CSS sheets and a ruby example file

---
+++ example_user.rb
@@ -0,0 +1,10 @@
+# exercise 3
+person1 = { :first => "John", :last => "Smith" }
+person2 = { :first => "Jane", :last => "Smith" }
+person3 = { :first => "Tommy", :last => "Smith" }
+
+params = { :father => person1 , :mother => person2 , :child => person3 }
+
+puts "#{params[:father][:first]}"
+puts "#{params[:mother][:last]}"
+
