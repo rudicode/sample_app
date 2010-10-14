fd2ac71985f8998764f1597716a5b950ccacd20f
R Luzar <rluzar@topmill.ca>
2010-10-14 18:45:50 -0400

    Issue 7 - added integration_sign_in from exercise 3.9.1 to refactor some test code

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -37,6 +37,7 @@ describe "Users" do
 	describe "sign in/out" do
 		describe "failure" do
 			it "should not sign a user in" do
+				# integration_sign_in(user)
         visit signin_path
         fill_in :email,    :with => ""
         fill_in :password, :with => ""
@@ -48,10 +49,11 @@ describe "Users" do
 		describe "success" do
 			it "should sign a user in and out" do
         user = Factory(:user)
-        visit signin_path
-        fill_in :email,    :with => user.email
-        fill_in :password, :with => user.password
-        click_button
+				integration_sign_in(user)
+        #visit signin_path
+        #fill_in :email,    :with => user.email
+        #fill_in :password, :with => user.password
+        #click_button
         controller.should be_signed_in
         click_link "Sign out"
         controller.should_not be_signed_in
